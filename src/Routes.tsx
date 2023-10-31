91440bc90a4f33b5ecce5241d2d10b6537b09d0e
JonahanComarella <jonathanwcomarella@gmail.com>
2023-10-30 21:17:23 -0300

    created Navbar and Footer

---
+++ src/Routes.tsx
@@ -0,0 +1,44 @@
+import {BrowserRouter} from 'react-router-dom'
+import Navbar from './core/components/Navbar';
+// import Auth from 'pages/Auth';
+// import Navbar from './core/components/Navbar';
+// import Admin from './pages/Admin';
+// import Catalog from './pages/Catalog';
+// import ProductDetails from './pages/Catalog/components/ProductDetails';
+import Footer from 'core/components/Footer';
+
+
+const Routes = () =>(
+  <BrowserRouter>
+    <Navbar/>
+    {/* <Switch> */}
+      
+      {/* <Route path="/" exact>
+      <Redirect from="/" to="/products" exact/>
+        <Catalog/>
+      </Route>
+      
+      <Route path="/products" exact>
+        <Catalog/>
+      </Route>
+      
+      <Route path="/products/:productId">
+        <ProductDetails/>
+      </Route>
+
+      <Redirect from="/auth" to="/auth/login" exact/>
+      <Route path="/auth">
+          <Auth/>
+      </Route>
+      
+      <Redirect from="/admin" to="/admin/products" exact/>
+      <Route path="/admin">  
+        <Admin/>
+      </Route> */}
+
+    {/* </Switch> */}
+    <Footer/>
+     </BrowserRouter>
+);
+
+export default Routes;
---
+++ src/core/components/Navbar/index.tsx
@@ -0,0 +1,135 @@
+// import { getAccessTokenDecoded, logout } from 'core/utils/auth';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Link, NavLink, useLocation } from 'react-router-dom';
+import './styles.scss';
+import menu from '../../assets/images/menu.svg';
+
+const Navbar = () => {
+  const [drawerActive, setDrawerActive] = useState(false);
+  const [currentUser, setCurrentUser] = useState('');
+  const location = useLocation();
+
+//   useEffect(() => {
+//     const currentUserData = getAccessTokenDecoded();
+//     setCurrentUser(currentUserData.user_name);
+//   }, [location])
+
+//   const handleLogout = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+//     event.preventDefault();
+//     logout();
+//   }
+
+  return (
+    <nav className="bg-primary main-nav">
+
+      <Link to="/dashboard" className="nav-logo-text">
+        <h4>MEUS PILA</h4>
+      </Link>
+      <button
+        className="menu-mobile-btn"
+        type="button"
+        onClick={() => setDrawerActive(!drawerActive)}
+      >
+        <img src={menu} alt="Mobile Menu" />
+      </button>
+
+      <div className={drawerActive ? "menu-mobile-container" : "menu-container"}>
+        <ul className="main-menu">
+          {
+            currentUser && ( 
+          <li>
+            <NavLink 
+              className="nav-link" 
+              onClick={() => setDrawerActive(false)} 
+              to="/dashboard"
+            >
+              DASHBOARD
+            </NavLink>
+          </li>
+            )}
+          {
+            currentUser && (
+              <li>
+                <NavLink 
+                  className="nav-link" 
+                  onClick={() => {setDrawerActive(false)}}
+                  to="/admin"
+                  >
+                  PAINEL DO ADMIN
+                </NavLink>
+              </li>
+            )}
+          
+          {
+            drawerActive && (
+              <li>
+                {
+                  currentUser && (
+                    <a 
+                      href="#logout" 
+                      className="nav-link active d-inline"
+                      onClick={(e) => {
+                        setDrawerActive(false)
+                        // handleLogout(e)
+                      }}
+                    >
+                      {`LOGOUT - ${currentUser}`}
+                    </a>
+                  )
+                }
+              </li>
+            )
+          }
+          {
+            drawerActive && (
+              <>
+                {!currentUser && (
+                  <li>
+                    <Link 
+                      to="/auth/login" 
+                      className="nav-link active"
+                      onClick={() => setDrawerActive(false)}
+                    >
+                      LOGIN
+                    </Link>
+                  </li>
+                )}
+
+              </>
+            )
+          }
+        </ul>
+      </div>
+
+      <div className="user-info-dnone text-right">
+        {currentUser && (
+          <>
+            {currentUser}
+            <a
+              href="#logout"
+              className="nav-link active d-inline"
+              onClick={(e) => {
+                setDrawerActive(false)
+                // handleLogout(e)
+              }}
+            >
+              LOGOUT
+            </a>
+          </>
+        )}
+        {!currentUser && (
+          <Link 
+            to="/auth/login" 
+            className="nav-link active" 
+            onClick={() => setDrawerActive(false)}
+          >
+            LOGIN
+          </Link>
+        )}
+      </div>
+    </nav>
+  )
+};
+
+export default Navbar;
